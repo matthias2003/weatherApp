a23834d8e64f8b986503e70c64d07a95b259ce1f
Maciej Kłoda <maciej.kloda98@gmail.com>
2022-01-26 18:54:56 +0100

    Js search fade in and fade out

--- src/main.js
+++ src/main.js
@@ -31,22 +31,63 @@ const connectHTMLElements = () => {
 const setupListeners = () => {
     viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
     viewElems.searchButton.addEventListener('click', onClickSubmit);
+    viewElems.returnToSearchBtn.addEventListener('click',returnToSearch);
 }
 
 const initializeApp = () => {
     connectHTMLElements();
     setupListeners();
 }
-
-
-const onClickSubmit = () => {};
-
-const onEnterSubmit = event => {
-    console.log(event);
-    if(event.key==='Enter') {
+const onClickSubmit = () => {
         let query = viewElems.searchInput.value;
         getWeatherByCity(query).then(data => {console.log(data)});
+        switchView();
+};
+
+const onEnterSubmit = event => {
+    if(event.key==='Enter') {
+        fadeInOut();
+        let query = viewElems.searchInput.value;
+        getWeatherByCity(query).then(data => {
+            console.log(data)
+            switchView();
+            fadeInOut();
+            
+        });
+        
     }
 };
+
+
+const fadeInOut = () => {
+    if (viewElems.mainContainer.style.opacity === '1' || viewElems.mainContainer.style.opacity === ''){
+        viewElems.mainContainer.style.opacity = '0';
+    } else {
+        viewElems.mainContainer.style.opacity = '1';
+    }
+};
+
+
+
+const switchView = () => {
+    if(viewElems.weatherSearchView.style.display !== 'none') {
+        viewElems.weatherSearchView.style.display = 'none'
+        viewElems.weatherForecastView.style.display = 'block';
+    }
+    else {
+        viewElems.weatherForecastView.style.display = 'none';
+        viewElems.weatherSearchView.style.display = 'flex';
+    }
+
+};
+
+const returnToSearch = () => {
+    fadeInOut();
+    setTimeout(()=>{
+        switchView();
+        fadeInOut()
+    },500);
+}
+
 
 document.addEventListener('DOMContentLoaded',initializeApp);
